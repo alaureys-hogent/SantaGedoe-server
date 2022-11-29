fcec0499bef48f1deca68fdd7ee1681c04ecd534
Arnaud Laureys <arnaud.laureys@student.hogent.be>
2022-11-29 20:14:05 +0100

    Cors issue

--- config/production.js
+++ config/production.js
@@ -4,7 +4,7 @@ module.exports = {
     disabled: false,
   },
   cors: {
-    origins: ['http://localhost:3000'],
+    origins: ['https://santagedoe-client.onrender.com/'],
     maxAge: 3 * 60 * 60, // 3h in seconds
   },
   database: {
